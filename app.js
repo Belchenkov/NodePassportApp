32db7ce6a4829a2baa8e50cbef207efbd2268ac2
Aleksey Belchenkov <u608110@gmail.com>
2017-01-30 20:42:08 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local');
+var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+
+
